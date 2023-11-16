51fe9cdcb7a9c489984452997c21e95478959169
Tunahan Yavuz <rey_tuna@hotmail.com>
2023-11-17 00:53:21 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;              //Değişkenleri atıyoruz.
+    double p;
+    printf("Bir Vektorun P-Normlarini Yazdiran Programa Hos Geldiniz\nLutfen Bir n ve P Degeri Giriniz.\n--------------------------------\n");
+
+    takeN:
+    if (scanf("%d", &n) != 1){
+        fflush(stdin);
+        printf("Sadece Sayi Giriniz\nLutfen n Degerini Tekrar Giriniz\n");
+        goto takeN;
+    }
+    takeP:
+    if (scanf("%lf", &p) != 1){
+        fflush(stdin);
+        printf("Sadece Sayi Giriniz\nLutfen p Degerini Tekrar Giriniz\n");
+        goto takeP;
+    }
+    if(n<=0||p==0){               //Hatalı tuşlama var mı diye kontrol ediyoruz.
+        if(n<=0)
+            printf("Hatali Tuslama Yaptiniz n Degeri 0 ve 0' dan Kucuk Olamaz.\n");
+        else
+            printf("P Degeri 0' a Esit Olamaz\n");
+        printf("Lutfen Degerleri Tekrar Giriniz");
+        goto takeN;
+    }
+    for (int i = 1; i <=n ; ++i) {
+        if(i==1)
+
+            printf("(x%d^%0.1lf",i,p);
+        else
+            printf("+x%d^%0.1lf",i,p);
+    }
+    printf(")^1/%0.1lf",p);
+    return 0;
+}
